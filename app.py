f80df068b68f3c161329a41464d95e7454678479
DongWonC <choidongwon9929@gmail.com>
2023-11-16 01:16:39 +0000

    UPDATE: plot() data

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def main():
     st.title("Hello GCP from Local")
     fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot([1, 2, 3])
+    ax.plot([1, 2, 3, 4, 3, 2, 1])
     ax.set_title("Hello Plot from Local PC")
     st.pyplot(fig)
 
