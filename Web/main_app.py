3ed16b27d828e14abf32b019f5ee83d84e3cbeff
DongWonC <choidongwon9929@gmail.com>
2023-11-23 22:22:17 +0900

    update: main_app_image

--- Web/main_app.py
+++ Web/main_app.py
@@ -1,13 +1,15 @@
 # -*- coding:utf-8 -*-
-
+import requests
 import streamlit as st
 from PIL import Image
 
-from sub_app import home_app, store_app, agree, message
+from sub_app import home_app, store_app, total
 def main():
 
     with st.sidebar:
-        sidebar_image = Image.open('./image/title.png')
+        sidebar_image_url = 'https://github.com/DongWonC/telecom/Web/image/kt_poster.png'
+        response = requests.get(sidebar_image_url)
+        sidebar_image = Image.opne(BytesIO(response.content))
         st.image(sidebar_image, use_column_width=True)
         
         menu = ['홈', '매장 찾기', '서비스 문자 발송', '서비스 제공자']
@@ -20,10 +22,13 @@ def main():
         store_app.main()
 
     elif choice == '서비스 문자 발송':
-        agree.main(message)
+        total.main()
 
     elif choice == '서비스 제공자':
-        st.image(".\image\procider.png")
+        provider_image_url = 'https://github.com/DongWonC/telecom/Web/image/procider.png'
+        provider_image_response = requests.get(provider_image_url)
+        provider_image = Image.open(BytesIO(provider_image_response.content))
+        st.image(provider_image)
     
 
 if __name__ == "__main__":
